1577ea5c84077a4813e772497aae685b486eed83
MARCOS ROBERTO JULIO <marcos.rjulio@senacsp.edu.br>
2024-09-18 17:07:44 -0300

    meu primeiro repositório

---
+++ 00-inicio/script.js
@@ -0,0 +1,5 @@
+console.log("olá mundo!");
+console.log("Meu nome é  Marcos! ");
+console.log( Hoje estou aprendendo  Lógica de programacão");
+alert("programador!");
+console.log("Eu sou um otímo programador!");
